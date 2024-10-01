6e78c252dad7d70fa6e0e414eba91d41dfe29123
Roman Joseph Gallardo <romanjosephgallardo01@gmail.com>
2024-10-01 16:20:19 +0800

    Remove docstrings.

--- cube_of_integers.py
+++ cube_of_integers.py
@@ -1,19 +1,4 @@
-'''
-Steps:
-1. Input the size of the array.
-2. input the elements of the array
-3. display the cube of each element
-
-Sample output
-
-Enter the size of the array : 3
-Enter the elements separated by space: 3 10 20
-27
-1000
-8000
-'''
-
-# pseudocode
+# Cube of integers:
 
 # Get the size of the array
 array_size = int(input("Enter the size of the array: "))
@@ -28,4 +13,5 @@ for numbers in entered_elements:
 # Calculate the cube of each element
 for cube in range(array_size):
     array[cube] = array[cube] ** 3
+    # Print the array
     print(array[cube])
