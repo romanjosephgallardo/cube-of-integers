df4e4bdb18101df53cc998a6723c6cc7240600d0
Roman Joseph Gallardo <romanjosephgallardo01@gmail.com>
2024-10-01 16:37:03 +0800

    Implement exception handlings for user inputs.

--- cube_of_integers.py
+++ cube_of_integers.py
@@ -1,10 +1,23 @@
 # Cube of integers:
 
 # Get the size of the array
-array_size = int(input("Enter the size of the array: "))
+try:
+    array_size = int(input("Enter the size of the array: "))
+except ValueError:
+    print("Invalid input. Please enter an integer.")
+    exit()
 
 # Get the elements of the array
-entered_elements = (input("Enter the elements separated by space: ")).split()
+while True:
+    entered_elements = (input("Enter the elements separated by space: ")).split()
+    if len(entered_elements) != array_size:
+        print(f"Invalid input. Please enter an integer with {array_size} elements seperated with spaces")
+        exit()
+    elif len(entered_elements) == array_size:  
+        break
+    else:
+        print("Ivalid input. Please enter an integer")
+        exit()
 
 # Convert the elements to distinct value of integers
 array = []
