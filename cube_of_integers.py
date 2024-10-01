ae2bce577257e26c6e10342e1c8ce871bdc9f7dd
Roman Joseph Gallardo <romanjosephgallardo01@gmail.com>
2024-10-01 15:52:11 +0800

    Add pseudocode

---
+++ cube_of_integers.py
@@ -0,0 +1,21 @@
+'''
+Steps:
+1. Input the size of the array.
+2. input the elements of the array
+3. display the cube of each element
+
+Sample output
+
+Enter the size of the array : 3
+Enter the elements separated by space: 3 10 20
+27
+1000
+8000
+'''
+
+# pseudocode
+
+# Get the size of the array
+# Get the elements of the array
+# Calculate the cube of each element
+# Display the cube of each element
