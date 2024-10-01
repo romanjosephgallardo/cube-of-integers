91175e088fce4c0d60916e0948a67a53b432dd5a
Roman Joseph Gallardo <romanjosephgallardo01@gmail.com>
2024-10-01 16:14:48 +0800

    Add implementation of the program

--- cube_of_integers.py
+++ cube_of_integers.py
@@ -16,6 +16,19 @@ Enter the elements separated by space: 3 10 20
 # pseudocode
 
 # Get the size of the array
+array_size = int(input("Enter the size of the array: "))
+
 # Get the elements of the array
+entered_elements = (input("Enter the elements separated by space: "))
+
+array = []
+for numbers in entered_elements.split():
+    array.append(int(numbers))
+
 # Calculate the cube of each element
+for cube in range(array_size):
+    array[cube] = array[cube] ** 3
+
 # Display the cube of each element
+for display_cube in range(array_size):
+    print(array[display_cube])
